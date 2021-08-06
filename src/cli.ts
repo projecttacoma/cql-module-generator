fd831bd997e839542b8a58b9be2efae360a77037
sarahmcdougall <smcdougall@mitre.org>
2021-08-06 10:08:56 -0400

    Added new cli

---
+++ src/cli.ts
@@ -0,0 +1,10 @@
+import { Command } from 'commander';
+import fs from 'fs';
+
+const program = new Command();
+program
+  .version('0.0.1')
+  .requiredOption('-b, --bundle <bundle>', 'path to bundle containing FHIR Libraries')
+  .parse(process.argv);
+
+const bundle = JSON.parse(fs.readFileSync(program.bundle, 'utf8'));
