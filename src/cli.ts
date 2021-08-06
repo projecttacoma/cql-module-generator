e3afaf93d5d5fddcfb5e7ef6374bdf0180680b14
sarahmcdougall <smcdougall@mitre.org>
2021-08-06 15:37:54 -0400

    added const for program opts

--- src/cli.ts
+++ src/cli.ts
@@ -2,9 +2,11 @@ import { Command } from 'commander';
 import fs from 'fs';
 
 const program = new Command();
+
 program
   .version('0.0.1')
   .requiredOption('-b, --bundle <bundle>', 'path to bundle containing FHIR Libraries')
   .parse(process.argv);
 
-const bundle = JSON.parse(fs.readFileSync(program.bundle, 'utf8'));
+const options = program.opts();
+const bundle = JSON.parse(fs.readFileSync(options.bundle, 'utf8'));
