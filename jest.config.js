e9c3ad2278c4ba9e360305e42e5763f49a9afef2
sarahmcdougall <smcdougall@mitre.org>
2021-08-10 11:56:58 -0400

    Added factory file and related tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  moduleFileExtensions: ['ts', 'js', 'd.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testMatch: ['**/test/**/*.test.(ts|js)'],
+  testEnvironment: 'node'
+};
---
+++ src/states/factory.ts
@@ -0,0 +1,40 @@
+// need updated ts states file from other task
+import * as states from './states';
+
+/**
+ * creates an instance of a specified class with default values
+ *
+ * @param {string} dataType - the dataType from the object returned by getDataType.ts
+ * @param {string} name - the name from the object returned by getDataType.ts
+ * @returns {BaseState} - instance of the state class from the data type with corresponding name
+ */
+export function factory(dataType: string, name: string): states.BaseState | null {
+  // don't worry about codes right now because the way we assemble the codes arg is going to change
+  // all code args currently set to [] in each state class
+  switch (dataType) {
+    case 'Encounter':
+      return new states.EncounterState(name, null, []);
+    case 'Condition':
+      return new states.ConditionOnsetState(name, null, []);
+    case 'AllergyIntolerance':
+      return new states.AllergyOnsetState(name, null, []);
+    case 'Medication':
+      return new states.MedicationOrderState(name, []);
+    case 'CarePlan':
+      return new states.CarePlanStartState(name, []);
+    case 'Procedure':
+      return new states.ProcedureState(name, []);
+    case 'ImagingStudy':
+      return new states.ImagingStudyState(name, null, []);
+    case 'Device':
+      return new states.DeviceState(name, null);
+    case 'SupplyDelivery':
+      return new states.SupplyListState(name, null);
+    case 'Observation':
+      return new states.ObservationState(name, null, null, []);
+    case 'DiagnosticReport':
+      return new states.DiagnosticReportState(name, null, []);
+    default:
+      return null;
+  }
+}
