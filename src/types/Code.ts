1abcce9fb08fecb179ae82c71793cd5337dd3346
mriley <mriley@mitre.org>
2021-08-11 10:13:51 -0400

    Corrected pathing issue with states.ts

--- src/types/Code.ts
+++ src/types/Code.ts
@@ -1,6 +1,6 @@
-export interface  code {
-    system: "SNOMED-CT" | "RxNorm" | "LOINC" | "NUBC" | "DICOM-DCM" | "DICOM-SOP",
-    code: string,
-    display: string,
-    value_set: string
-  }
+export interface code {
+  system: 'SNOMED-CT' | 'RxNorm' | 'LOINC' | 'NUBC' | 'DICOM-DCM' | 'DICOM-SOP';
+  code: string;
+  display: string;
+  value_set: string;
+}
