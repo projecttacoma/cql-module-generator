349f9fd7be2b297436862e45510b0c6f8923b418
natjoe4 <35576465+natjoe4@users.noreply.github.com>
2021-08-18 15:34:13 -0400

    Update src/ExportModule.ts
    Co-authored-by: hossenlopp <hossenlopp@mitre.org>

--- src/ExportModule.ts
+++ src/ExportModule.ts
@@ -36,7 +36,7 @@ export function exportModule(libName: string, dataTypes: CalculatorTypes.DataTyp
     if (object.dataType !== null && factory(object.dataType, stateName) !== null) {
       const stateClass = factory(object.dataType, stateName);
       if (object.valueSet !== undefined && stateClass && states.doesHaveCodes(stateClass)) {
-        stateClass['codes'].push({
+        stateClass.codes.push({
           system: '',
           code: '',
           display: '',
